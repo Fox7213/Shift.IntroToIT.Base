e4415d1e13b97c64cec187d420d5f3cfbcf6fa08
Fox7213 <121626688+Fox7213@users.noreply.github.com>
2023-10-12 20:57:37 +0500

    Update Task.py
    refactor:перевернула строку

--- IntroToIT/lesson2/Task2/Task.py
+++ IntroToIT/lesson2/Task2/Task.py
@@ -1,6 +1,6 @@
 #INTRO TO IT 2nd COURSE
 # Задача 2: Переверни строку!
 # Ученик, твоя задача перевернуть строку, используя срезы в Python.
-def перевернуть_строку(строка):
+def reversed_string(строка):
     return строка[::-1]
-print(перевернуть_строку("Python"))
+print(reversed_string("Python"))
